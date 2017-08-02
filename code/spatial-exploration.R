f50de2398716852e814023437d832a0a58dfbbd7
amschatz <amschatz@users.noreply.github.com>
2017-08-02 08:50:52 -0400

    pausing work on spatial vars

--- code/spatial-exploration.R
+++ code/spatial-exploration.R
@@ -1,6 +1,6 @@
 #data and packages
 muni.map<-readShapePoly("../data_clean/BRAZpolygons.shp") # @data has muni # and name
-yf.long<-readRDS("../data_clean/YFlong.rds")
+yf.long<-readRDS("../data_clean/YFcases/YFlong.rds")
 library(maptools)
 library(shapefiles)
 library(rgeos)
@@ -45,6 +45,10 @@ p<-unique(as.numeric(yf.long1$muni.no))
 #########################
 #find distance to nearest infected muni in the same month
 #########################
+
+#add dummy var for whether or not there is another infected muni
+#if so, find distance
+#if not, set distance to 0
 
 dist.inf<-as.data.frame(matrix(nrow=nrow(yf.long1), ncol=7))
 colnames(dist.inf)<-c("year", "month", "month.no", "focal.muni.no", "distance", "inf.muni.no", "cases.no")
@@ -97,4 +101,53 @@ for (i in unique(yf.long1$month.no)) { #for each month.no
 #find distance to nearest infected muni in the previous month
 #########################
 
+#add dummy var for whether or not there is any infected muni in the previous month
+#if so, find distance (including 0 for distance to self)
+#if not, set distance to 0
 
+dist.prev<-as.data.frame(matrix(nrow=nrow(yf.long1), ncol=7))
+colnames(dist.prev)<-c("year", "month", "month.no", "focal.muni.no", "distance", "inf.muni.no", "cases.no")
+
+month.index<-1
+
+for (i in unique(yf.long1$month.no)) { #for each month.no
+  #limit case data to month i
+  yf<-yf.long1[as.numeric(yf.long1$month.no)==i,]
+  
+  #vectors for data
+  focal.muni.no<-rep(NA,ncol(c.dist))
+  distance<-rep(NA,ncol(c.dist))
+  inf.muni.no<-rep(NA,ncol(c.dist))
+  cases.no<-rep(NA,ncol(c.dist))
+  
+  for (j in 1:ncol(c.dist)) { #for each muni with distance data
+    #attach muni#s to centroid distances for muni j
+    dist<-as.data.frame(cbind(as.numeric(as.character(cent.df@data$muni_no)), as.numeric(c.dist[,j])))
+    colnames(dist)<-c("muni.no", "dist")
+    #combine spatial and case data
+    dist.case<-join(as.data.frame(dist), yf, by="muni.no")
+    #remove current focal muni and entries with NA cases
+    dist.case1<-dist.case[dist.case$dist!=0 & !is.na(dist.case$case),]
+    
+    #find min dist to muni with cases (if there isn't one, insert NA)
+    distance[j]<-ifelse(nrow(dist.case1)>0, min(dist.case1$dist), NA)
+    #pull other relevant info
+    inf.muni.no[j]<-ifelse(nrow(dist.case1)>0, dist.case1[dist.case1$dist==distance[j],1], NA)
+    cases.no[j]<-ifelse(nrow(dist.case1)>0, dist.case1[dist.case1$dist==distance[j],6], NA)
+    focal.muni.no[j]<-dist[j,1]
+  }
+  
+  #save data
+  row.start<-((month.index-1)*5564)+1
+  row.end<-row.start+5563
+  dist.prev$year[row.start:row.end]<-yf$year
+  dist.prev$month[row.start:row.end]<-yf$month
+  dist.prev$month.no[row.start:row.end]<-yf$month.no
+  dist.prev$focal.muni.no[row.start:row.end]<-focal.muni.no
+  dist.prev$distance[row.start:row.end]<-distance
+  dist.prev$inf.muni.no[row.start:row.end]<-inf.muni.no
+  dist.prev$cases.no[row.start:row.end]<-cases.no
+  
+  month.index<-month.index+1
+  
+}
