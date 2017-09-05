c742b66cb7640311adda11e7b19b28798162084c
DrakeLab <tad.dallas@drakeresearchlab.com>
2017-09-05 18:18:52 -0400

    create fire processing script and add metadata

---
+++ data_processingScripts/fireProcess.R
@@ -0,0 +1,57 @@
+## This file processes the point fire data into data by muni and month
+## MV Evans 2017-09-05
+
+# Data was downloaded from LIST WEBSITE. Becasue the file was too large to load on most computers, it was subset into yearly data for later processing
+
+#packages
+library(raster)
+library(rgeos)
+library(rgdal)
+library(gdalUtils)
+library(doParallel)
+library(foreach)
+library(sp)
+library(lubridate)
+
+#load brazil shapefile
+brazil <- readOGR(dsn="../data_clean", layer="BRAZpolygons")
+#project properly (although it shouldn't matter)
+brazil <- spTransform(brazil, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+fireFiles <- list.files("../../fireSubsets", pattern=".shp", full.names=T)
+
+#set up workers
+cl <- makeCluster(18)
+registerDoParallel(cl)
+#read in each file and then get zonal statistics of sum of points within a municipality
+system.time({
+  fires <- foreach(i=1:length(fireFiles), .combine=cbind, .packages=c("lubridate","sp","raster", "rgdal", "gdalUtils")) %dopar% {
+    fireSp <- readOGR(fireFiles[i])
+    #only keep confident points >80, based on how MODIS calculates pixels (https://cdn.earthdata.nasa.gov/conduit/upload/3865/MODIS_C6_Fire_User_Guide_A.pdf)
+    fireSp <- fireSp[fireSp@data$CONFIDENCE>=80,]
+    fireSp <- spTransform(fireSp, CRS(brazil))
+    fireSp@data$Date <- as.Date(as.character(fireSp@data$ACQ_DATE), format="%Y/%m/%d")
+    fireSp@data$Year <- year(fireSp@data$Date)
+    fireSp@data$Month <- month(fireSp@data$Date)
+    #now loop over the month
+    for (j in 1:12){
+      monthFire <- fireSp[fireSp@data$Month==j,]
+      #get number of fires per muni in that month
+      monthTotal <- colSums(gContains(brazil, monthFire, byid = TRUE))
+      if (j==1){
+        yearFires <- monthTotal
+      } else yearFires <- cbind(yearFires, monthTotal)
+    }
+    
+    colnames(yearFires) <- paste0(2000+i, "month", 01:12)
+    write.csv(yearFires, paste0("../../fireSubsets/CSVs/", fire, 2000+i, ".csv"), row.names = F)
+    return(yearFires)
+  }
+  stopCluster(cl)
+})
+
+#turn into big dataframe
+firesDF <- as.data.frame(firesDF)
+firesDF$muni.no <- brazil@data$muni_no
+firesDF2 <- firesDF[,c(169, 1:168)]
+write.csv(firesDF2, file="../data_raw/environmental/fires.csv")
--- data_raw/environmental/EnvCovar-metadata.md
+++ data_raw/environmental/EnvCovar-metadata.md
@@ -12,11 +12,9 @@
 
 **Downloaded**: From MODIS, following `ModisDownload.R` script
 
-**Spatial & Temporal Resolution**: Began as 1 km/8 days, processed into municipality/month via 'extract.R' script
+**Spatial & Temporal Resolution**: Began as 5 km/month days, processed into mean Temperature/municipality/month via 'extract.R' script
 
-**Sub-data?**: Processed into min, mean, and max by first taking the min, mean and maximum temperatures per 1 km grid cell per month. Then spatially aggregated to municipality by min, mean and max.
-
-**Filename**: maxTall.csv, minTall.csv, meanTall.csv
+**Filename**: meanTall.csv
 
 ## Land Cover
 
@@ -32,6 +30,14 @@
 
 **Downloaded**: From Tropical Rainfall Measuring Mission, downloaded via command line using `trmmDownload.sh` script (can run through R). Pay attention to notes in comments at top of script or it will not work.
 
-**Spaital & Temporal Resolution**: Began as 4 km/month (recorded as average hourly rainfall in mm), recorded into spatial min. mean, and maximums per municipality/month
+**Spatial & Temporal Resolution**: Began as 4 km/month (recorded as average hourly rainfall in mm), recorded into spatial min. mean, and maximums per municipality/month
 
-**Filename**: minRFall.csv, maxRFall.csv, meanRFall.csv
+**Filename**: meanRFall.csv
+
+## Fire
+
+**Downloaded**:
+
+**Spatial and Temporal Resolution**: Vector data (i.e. points) by date and time. Combined into number of fires per municipality per month via `fireProcess.R`
+
+**Filename**:
