d3854ac8933231757a2243486323a5825f3f5a6d
Reni Kaul <reni@uga.edu>
2017-09-19 17:28:39 -0400

    beginning of script to load ALL the data into workspace

---
+++ data_processingScripts/GiantDataFrameMaker.R
@@ -0,0 +1,24 @@
+#script reads in all the data, assembles it and removes everything but the final dataset
+
+#1. Load packages
+
+library(dplyr)
+library(gplots)
+library(parallel)
+library(doParallel)
+library(ROCR)
+library(pROC)
+
+#2. Load data
+
+
+
+#3. Assemble data
+
+
+#4. Calculate additional covar
+
+
+#5. Transform covar as needed
+
+#6. Rm everything but
