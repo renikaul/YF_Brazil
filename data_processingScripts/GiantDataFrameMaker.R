8fa5965f2ae957a27e66153787501bcbd39f3ca7
Reni Kaul <reni@uga.edu>
2017-10-19 17:40:17 -0400

    data assembled by GiantDataFrameMaker.r

--- data_processingScripts/GiantDataFrameMaker.R
+++ data_processingScripts/GiantDataFrameMaker.R
@@ -1,5 +1,26 @@
-#script reads in all the data, assembles it and removes everything but the final dataset
-#Covarate information in comments of load data
+#script reads in all the data, assembles it and saves the final dataset in data_clean/FinalData.rds
+#Covarate information 
+
+# case : number of reported YF cases
+# NDVI : NDVI for that month
+# NDVIScale : NDVI rescaled to max value for that muni and calendar month
+# muniArea : km2
+# popLog10 : population density 
+# RF : mean hourly rainfall
+# RFsqrt : mean hourly rainfall sqrt transformed
+# RFScale : mean hourly rainfall rescaled to max value for that muni and calendar month
+# tempMean : mean monthly air temperature 
+# tempScale : mean monthly air temperature rescaled to max value for that muni and calendar month
+# numFire : number of fires oberserved in month
+# fireDens : number of fires oberserved in month divided by muniArea
+# spRich : number of non-human primates by species with ranges based on IUCN {0-22}
+# primProp : sum of each municipalities relative area that is both agricultural and falls within a primate genus range. {0,9} Missing for 2014
+# muni.no : unique number to identify municipality
+# month.no : numbered month of observation {1,168}
+# muni.name : character string name
+# month : abbrevated calendar month name
+# cal.month : calendar month {1,12}
+# year : year {2001,2014}
 
 #1. Load packages-------------------------
 
@@ -34,6 +55,7 @@ rf <- readRDS("../data_clean/environmental/allRainfall.rds")
 
 #scale to maximum value of muni
 rf <- rf %>%
+  mutate(RF=hourlyRainfallMean) %>%
   mutate(RFsqrt=sqrt(hourlyRainfallMean)) %>%
   group_by(muni.no) %>%
   mutate(RFScale = hourlyRainfallMean/max(hourlyRainfallMean)) %>% #scale rainfall to muni
@@ -64,52 +86,66 @@ temp <- temp %>%
 
 ## Non-human primates------------------------------ 
     #primProp
-NHP <- readRDS("../data_clean/environmental/primProp.rds")
+NHPProp <- readRDS("../data_clean/environmental/primProp.rds") 
     #hist(sqrt(NHP$primProp))
       #transformations are ugly too 
+
+NHPRichness <- readRDS("../data_clean/environmental/primRichness.rds")
 ## Fires ----------------------------------
 
 fires <- readRDS("../data_clean/environmental/numFires.rds")
   #hist(fires$numFire) #super overdispersed, only about 20% muniXmonth have had a fire. 6% of muniXmonths have only had a single fire. The upper bound is >5000 per a muniXmonth. 
   #standard transformation really can correct for this extreme 
+  
+# fires <- fires %>%
+#   mutate(firesDenLog=log(fireDens)) %>%
+#   group_by(muni.no, cal.month) %>%
+#   mutate(firesDenScale = fireDens/max(fireDens)) %>% #scale fire densities to muni and month
+#   ungroup()
 
 ## YF cases------------------------------ 
 cases <- readRDS("../data_clean/YFcases/YFlong.rds")
+
+cases[is.na(cases)] <- 0 #replace NA with zero  
 
 cases <- cases %>%
   #drop annual totals
   filter(cal.month<=12) %>%
   #drop unknown origin totals %>%
-  filter(!grepl("ignorado", muni))
+  filter(!grepl("ignorado", muni)) 
 
 
 #3. Assemble data--------------------------
 
   #Put all the things together!  #Need to add fires after fix. 
-all.data <- plyr::join_all(list(ndvi, pop, rf, temp, cases), by=c("muni.no", "month.no"), type="full") %>%
-        select(muni.no, muni.name, year, cal.month, month.no, case, popLog10, NDVI,NDVIScale, RFsqrt,RFScale, tempMean, tempScale, muniArea) 
+all.data <- plyr::join_all(list(ndvi, pop, rf, temp, cases, fires), by=c("muni.no", "month.no"), type="full") 
 
-all.data <-  all.data %>%
-              left_join(NHP, by=c("muni.no", "year")) %>% 
-              mutate(muni.name= muni.name.x) %>%
-              select(-c(muni.name.y, muni.name.x)) #drop second muni.name
-    # #clean it up
-    # all.data <- all.data %>%
-    #   #drop if doesn't match envCovariates (these are all the ones that were emancipated in 2013)
-    #   filter(!is.na(muni.name)) %>%
-    #   #get only columns we need
-    #   select(muni.no, muni.name, year, cal.month, month.no, case, popDensity, NDVI,scale.ndvi, log.mean.rf,scale.mean.rf, tempMax, tempMean)
-    # 
-    # #turn NA cases to 'true' zeros
-    # all.data$case[is.na(all.data$case)] <- 0
-    # #all positive cases to 1s
-    # all.data$case[all.data$case>0] <- 1
-    # 
-    # #remove 291992 Madre de Deus; issues with NA in NDVI
-    # all.data <- all.data[complete.cases(all.data),]
+# add in NHP for each year and muni
+# all.data <-  all.data %>%
+#               left_join(NHP, by=c("muni.no", "year")) %>% 
+#               mutate(muni.name= muni.name.x) %>%
+#               select(-c(muni.name.y, muni.name.x)) #drop second muni.name
+
+#add in NHP Richness for each muni
+all.data2 <- all.data %>%
+            dplyr::left_join(NHPRichness)
+
+all.data2 <- all.data2 %>%
+            plyr::join(NHPProp)
 
 
+#4. Clean it up -------------
 
-#4. Calculate additional covar-------------
+final.data <- all.data2[c("case",
+                 "NDVI","NDVIScale",
+                 "muniArea","popLog10",
+                 "RF","RFsqrt","RFScale",
+                 "tempMean","tempScale",
+                 "numFire","fireDens",
+                 "spRich","primProp",
+                 "muni.no", "month.no", "muni.name", "month", "cal.month","year")]
 
-#5. Rm everything but----------------------
+
+#5. Save the data ----------------------
+
+saveRDS(final.data, file="../data_clean/FinalData.rds")
