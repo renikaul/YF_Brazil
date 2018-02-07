da9debec9409323b96ceeb3cf0ea9e27ab1613b5
Reni Kaul <reni@uga.edu>
2018-02-07 17:48:13 -0500

    most recent MS figures

--- code/MS_figures.R
+++ code/MS_figures.R
@@ -6,9 +6,7 @@
 #global packages
 library(dplyr)
 library(ggplot2)
-# ROC plots ----
-
-
+library(ggthemes)
 # Dodged Variable Importance ----
 
 #1. load packages and functions
@@ -17,10 +15,12 @@ library(ggplot2)
 RankImp=function(x){
   #pull out Relative Importance
   RI <- x[[1]]
+  #convert varImp to numeric (coming out as factor for some reason)
+  RI$varImp <- as.numeric(as.character(RI$varImp))
   #order and add rank
   RI <- RI %>%
     dplyr::arrange(desc(varImp)) %>%
-    dplyr::mutate(varRank=1:dim(RI)[1])
+    dplyr::mutate(varRank=dim(RI)[1]:1)
   
   return(RI)
 }
@@ -28,11 +28,14 @@ RankImp=function(x){
 
 #2. Load data
 #Whole Country Model 
-fullM <- readRDS("../data_out/SpaceAndTimeSplit/Trim2014/Perm10FullModel500TryCatch.rds") 
+#fullM <- readRDS("../data_out/SpaceAndTimeSplit/Trim2014/Perm10FullModel500TryCatch.rds") 
 
 #NHP Split Model
-lowM <- readRDS("../data_out/TwoModelSoln/Trim2014/LPermFullModelTryCatch.rds")
-highM <- readRDS("../data_out/TwoModelSoln/Trim2014/HPermFullModelTryCatch.rds")
+#lowM <- readRDS("../data_out/TwoModelSoln/Trim2014/LPermFullModelTryCatch.rds")
+#highM <- readRDS("../data_out/TwoModelSoln/Trim2014/HPermFullModelTryCatch.rds")
+fullM <- readRDS("../data_out/MS_results/OneModel/Perm100FullModel500TryCatch.rds") 
+lowM <- readRDS("../data_out/MS_results/LowModel/lPerm100Model500TryCatch.rds")
+highM <- readRDS('../data_out/MS_results/HighModel/HPerm100Model500TryCatch.rds')
 
 #3. Pull out variable importance from permutations
 Full <- RankImp(fullM)
@@ -47,9 +50,9 @@ RankFull$Model <- factor(c(rep("Full", dim(Full)[1]),
 #order the variable levels
 RankFull <- within(RankFull,
                    Variable <- factor(Variable, 
-                                      levels = c( "spRich", "popLog10", "RFsqrt","tempMean","NDVIScale","RF","primProp","RFScale","tempScale","NDVI", "fireDenScale", "fireDenSqrt")))
+                                      levels = c( "fireDenSqrt","NDVI", "fireDenScale","RFScale","primProp", "tempScale","NDVIScale","tempMean", "RFsqrt","spRich", "popLog10" )))
 
-RankFull$varImp <- as.numeric(as.character(RankFull$varImp))
+#RankFull$varImp <- as.numeric(as.character(RankFull$varImp))
 
 #Relative Imp Plots
 
@@ -57,5 +60,40 @@ b <- ggplot(RankFull, aes(x=Variable, y=varImp,fill=Model)) +
   geom_bar(stat="identity",position="dodge") +
  #facet_grid(Model~.) +
   ylab("Relative Importance") + xlab("variable") +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
+  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  theme_minimal()+
+  scale_fill_tableau("colorblind10")
+  
 plot(b)
+
+# Coord Plot ----
+ ##must run Variable importance plot first
+#cord plot business
+RankFull <- within(RankFull,
+                   Variable <- factor(Variable, 
+                                      levels = c( "fireDenSqrt","NDVI", "fireDenScale","RFScale","primProp", "tempScale","NDVIScale","tempMean", "RFsqrt","spRich", "popLog10" )))
+
+
+y_levels <- levels(RankFull$Variable)
+
+p <- ggplot(RankFull, aes(x = Model, y = varRank, group = Variable)) +   # group = id is important!
+  geom_path(aes(color = Variable),lineend = 'round', linejoin = 'round') +
+  scale_y_discrete(limits = levels(RankFull$Variable)) + #ylim/lab details
+  ylab("Rank Importance (1 is most important)") +
+#  scale_x_discrete(labels = c('Four','Six','Eight')) +
+  xlab("Model")+
+  annotate("text", x = c(0.75), y=c(1,2,3,4), label = c("two", "ship", "six", "boat")) + 
+  theme(legend.position = "none") 
+
+plot(p)
+
+
+
+
+
+library(tidyverse)
+
+mtcars <- within(mtcars, cyl <- factor(cyl, levels=c(4,6,8)))
+ggplot(mtcars) + 
+  geom_boxplot(aes(factor(cyl), mpg))+ 
+ scale_x_discrete(labels = c('Four','Six','Eight'))
