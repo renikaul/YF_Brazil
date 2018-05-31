5bb46cd22c5722dc2f070ff228bf02e057993966
mvevans89 <mvevans89@gmail.com>
2018-05-31 08:15:02 -0400

    overwrite brazil polygon layer to include merging of pinto bandeira and bento goncalves

--- data_processingScripts/combineMuniShapefiles.R
+++ data_processingScripts/combineMuniShapefiles.R
@@ -1,5 +1,6 @@
 ## This document combines all the municipality shapefiles that are currently by state, downloaded from IBGE, into one for Brazil.
 # Michelle V Evans
+
 library(rgdal)
 library(raster)
 library(rgeos)
@@ -42,3 +43,34 @@ colnames(brazShapefile@data)[2] <- "muni.name"
 
 
 writeOGR(brazShapefile, dsn=".", layer="BRAZpolygons", driver="ESRI Shapefile")
+
+
+## Additional Correction (2018-05-31) ----
+
+# Because we are using shapefiles that correspond to 2001 municipality boundaries, we must combine 
+# Pinto Bandeira (431454) and Bento Goncalves (430210) into just Bento Goncalves.
+
+# load incorrect shapefile 
+braz.shape <- readOGR("../data_clean", "BRAZpolygons")
+
+# adjust Pinto muni.no to equal Bento
+braz.shape$muni_no[braz.shape$muni_no==431454] <- 430210
+
+#dissolve
+braz.Dissolve <- unionSpatialPolygons(braz.shape, IDs=braz.shape$muni_no)
+# add muni nos in
+dissolve.muni <- data.frame(muni.no = names(braz.Dissolve))
+rownames(dissolve.muni) <- names(braz.Dissolve)
+braz.diss <- SpatialPolygonsDataFrame(braz.Dissolve, dissolve.muni)
+
+# check with model Munis
+preds <- readRDS("../data_out/MS_results/OneModel/wholePredictions.rds")
+modelMunis <- preds$muni.no
+modelMunis[!(modelMunis %in% braz.diss$muni.no)]
+braz.diss$muni.no[!(braz.diss$muni.no %in% modelMunis)]
+
+#if good, then save
+
+
+
+
